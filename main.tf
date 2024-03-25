897ab38c9993e75d7281655b33c5fd3f7ebac4ca
ucheenyi <ucheenyi@gmail.com>
2024-03-25 21:39:55 +0100

    Added kubeconfig Authentication code

--- main.tf
+++ main.tf
@@ -4,9 +4,7 @@ terraform {
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
-      
     }
-    
   }
 }
 
@@ -14,13 +12,39 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+/*
+## Kubeconfig
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+*/
+
+
+## Kubeconfig Authentication
+data "digitalocean_kubernetes_cluster" "uc-cluster-details" {
+  name = digitalocean_kubernetes_cluster.doks_cluster.name
+}
+
+provider "kubernetes" {
+  host  = data.digitalocean_kubernetes_cluster.uc-cluster-details.endpoint
+  token = data.digitalocean_kubernetes_cluster.uc-cluster-details.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.uc-cluster-details.kube_config[0].cluster_ca_certificate
+  )
+}
+
+
+
+
 
 ## Create the DOK Cluster:
-
 resource "digitalocean_kubernetes_cluster" "doks_cluster" {
   name     = var.cluster_name
   region   = var.region
   version  = var.k8s_version
+
   node_pool {
     name       = var.node_name
     size       = var.node_size
@@ -31,29 +55,34 @@ resource "digitalocean_kubernetes_cluster" "doks_cluster" {
 }
 
 ## Deploy the Static Route Controller:
-
 resource "kubernetes_deployment" "static_route_controller" {
   metadata {
     name      = "uc-static-route-controller"
     namespace = "kube-system"
   }
+
   spec {
-    replicas = 1
+    replicas = 2
+
     selector {
       match_labels = {
         app = "uc-static-route-controller"
       }
     }
+
     template {
       metadata {
         labels = {
           app = "uc-static-route-controller"
         }
       }
+
       spec {
         container {
           name  = "uc-static-route-controller"
-          image = "quay.io/replicated/static-route-controller:latest"
+          image = "hashicraft/minecraft:v1.16.3"
+
+          //image = "quay.io/replicated/static-route-controller:latest"
           args  = ["--kubeconfig=/etc/kubernetes/admin.conf"]
         }
       }
@@ -61,24 +90,23 @@ resource "kubernetes_deployment" "static_route_controller" {
   }
 }
 
-##Configure the Static Route Controller as an Egress Gateway:
-
+## Configure the Static Route Controller as an Egress Gateway:
 resource "kubernetes_service" "egress_gateway" {
   metadata {
     name      = "uc-egress-gateway"
     namespace = "kube-system"
   }
+
   spec {
     selector = {
       app = "uc-static-route-controller"
     }
+
     port {
       port        = 80
       target_port = 80
     }
+
     type = "LoadBalancer"
   }
 }
-
-
-
