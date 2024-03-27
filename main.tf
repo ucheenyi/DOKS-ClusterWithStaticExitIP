dc229b9f86971216cddea6d48bb660df5eeadb7a
ucheenyi <ucheenyi@gmail.com>
2024-03-27 14:56:19 +0100

    Added Provider file

--- main.tf
+++ main.tf
@@ -1,26 +1,6 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "digitalocean" {
-  token = var.do_token
-}
 
 data "digitalocean_kubernetes_cluster" "uc-cluster-details" {
   name = digitalocean_kubernetes_cluster.doks_cluster.name
-}
-
-provider "kubernetes" {
-  host  = data.digitalocean_kubernetes_cluster.uc-cluster-details.endpoint
-  token = data.digitalocean_kubernetes_cluster.uc-cluster-details.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    data.digitalocean_kubernetes_cluster.uc-cluster-details.kube_config[0].cluster_ca_certificate
-  )
 }
 
 
---
+++ providers.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+provider "kubernetes" {
+  host  = data.digitalocean_kubernetes_cluster.uc-cluster-details.endpoint
+  token = data.digitalocean_kubernetes_cluster.uc-cluster-details.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.uc-cluster-details.kube_config[0].cluster_ca_certificate
+  )
+}
